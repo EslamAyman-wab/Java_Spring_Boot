10dca9b232c57cc3bedbf976909a573f66864531
eslam <eslamayman2372002ii@gmail.com>
2026-04-16 11:20:04 +0200

    Initialize project configuration files and add .gitignore

---
+++ src/test/java/com/elsam/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsam.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
